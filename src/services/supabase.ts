d60cc89a8f2807489284beb5fdba73a7178101a7
aditi <abhishekchoudhary3139@gmail.com>
2025-06-07 18:28:13 +0530

    Minor change

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://ridgbwplpgurbmowxrlv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZGdid3BscGd1cmJtb3d4cmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg3NjA3NjUsImV4cCI6MjA2NDMzNjc2NX0.U93QBi5J9Cgp3R_6fP6a6DkgbNdXQzpgETa6x7aXxPQ";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey =import.meta.env.VITE_SUPABASE_ANON_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
